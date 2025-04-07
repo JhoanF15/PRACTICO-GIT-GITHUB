f9c021c660a9e9ddcd9fcc368b5df60ecbaee58f
JhoanF15 <jhoancarlos240804@gmail.com>
2025-04-07 17:44:37 +0200

    comit padre

--- Documents/NetBeansProjects/ProyectoGIt/src/proyectogit/ProyectoGIt.java
+++ Documents/NetBeansProjects/ProyectoGIt/src/proyectogit/ProyectoGIt.java
@@ -14,7 +14,8 @@ public class ProyectoGIt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+            
+        System.out.println("hola que tal");
     }
     
 }
